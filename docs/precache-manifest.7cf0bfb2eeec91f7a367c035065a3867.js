401413db08ee0097f89b1d4c502abdb3ef31a594
Fabiola Contreras <fabicon_2882@hotmail.com>
2020-09-06 18:48:09 -0500

    add docs

---
+++ docs/precache-manifest.7cf0bfb2eeec91f7a367c035065a3867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f73a84bbdf604a5967848c67c1fbb7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc624c7a8ec8073d408f",
+    "url": "/static/css/main.0d2051d8.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc624c7a8ec8073d408f",
+    "url": "/static/js/main.51281ae0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
